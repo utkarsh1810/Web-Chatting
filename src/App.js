6691792a7ecfa51b8091a3d11ee70d8987d1757d
utkarsh1810 <utkarshverma782@gmail.com>
2020-05-04 18:43:16 +0530

    adding ui of the web-chat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const users = [{ username: 'Amy' }, { username: 'John' }];
+
+const messages = [
+  { username: 'Amy', text: 'Hi, Jon!' },
+  { username: 'Amy', text: 'How are you?' },
+  { username: 'John', text: 'Hi, Amy! Good, you?' },
+];
+
+class App extends Component {
+
+  isDisabled = () => {
+    return false;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <div className="chat-window">
+            <h2>Super Awesome Chat</h2>
+            <div className="name sender">{users[0].username}</div>
+
+            <ul className="message-list">
+              {messages.map((message, index) => (
+                <li
+                  key={index}
+                  className={
+                    message.username === users[0].username ? 'message sender' : 'message recipient'
+                  }
+                >
+                  <p>{`${message.username}: ${message.text}`}</p>
+                </li>
+              ))}
+            </ul>
+
+            <div>
+              <form className="input-group">
+                <input type="text" className="form-control" placeholder="Enter your message..." />
+                <div className="input-group-append">
+                  <button className="btn submit-button" disabled={this.isDisabled()}>
+                    SEND
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          <div className="chat-window">
+            <h2>Super Awesome Chat</h2>
+            <div className="name sender">{users[1].username}</div>
+            <ul className="message-list">
+              {messages.map((message, index) => (
+                <li
+                  key={index}
+                  className={
+                    message.username === users[1].username ? 'message sender' : 'message recipient'
+                  }
+                >
+                  <p>{`${message.username}: ${message.text}`}</p>
+                </li>
+              ))}
+            </ul>
+
+            <div>
+              <form className="input-group">
+                <input type="text" className="form-control" placeholder="Enter your message..." />
+                <div className="input-group-append">
+                  <button className="btn submit-button" disabled={this.isDisabled()}>
+                    SEND
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
